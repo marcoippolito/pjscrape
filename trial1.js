e806d2e83084c7b716ebe662431fa47cf3ed17e3
Ubuntu <ubuntu@ip-10-164-57-85.ec2.internal>
2013-09-13 13:11:06 +0000

    trial1.js

--- trial1.js
+++ trial1.js
@@ -3,39 +3,30 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
+var csv = require('csv');
 // the scope of JS functions is defined at the time the function is created.//
 //for (pool in pools) {
 var url = 'http://www.ucimu.it/catalogo/imprese/elenco/';
     // self-executing function. While a callback function is a function created now but runs later, a self-executing function is created and immediately executed.
     // Whatever this function will returns will be the result of the while expression
-var companies = [];
+// var companies = [];
 request(url, ( function() {
+  var companies = [];
   return function(err, resp, body) {
     if (err)
       throw err;
     $ = cheerio.load(body);
     $('a[href^="catalogo/imprese"]').each(function(i, elem) {
-      companies[i] = $(this).text();
-      console.log($(this).text());
-      str = companies[i];
-      fs.appendFile('ucimu1.txt', 'str', function (err) {
-	if (err) throw err;
-	console.log('data appended');
+//      companies.push($(this).text());
+      console.log($(this).text())|
+      fs.writeFile('ucimu1.txt',$(this).text(), function (err) {
+        if (err) throw err;
       });
+//      console.log(companies.length);
     });
   };
+  return companies;
 })());
-//console.log(companies);
-// output: file text
-
-//var fmt = function(companies_array) {
-//  companies_array.join(",");
-//};
-
-//var outfile = 'ucimu1.txt';
-//var out = fmt(companies);
-//console.log(out);
-//fs.writeFile(outfile,out, function (err)) {
-//  if (err) throw err;
-//  console.log('It\'s saved!');
-//});
+//console.log(companies.length);
+// fetch data from level-2 sites //
+// for (var i = 1; i <= length(companies)
