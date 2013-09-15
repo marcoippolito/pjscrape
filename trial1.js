0417f7cbfddbbbbf1ba2c48cd25fb8e828ac2865
Ubuntu <ubuntu@ip-10-164-57-85.ec2.internal>
2013-09-15 07:50:22 +0000

    trial1.js

--- trial1.js
+++ trial1.js
@@ -7,9 +7,10 @@ var csv = require('csv');
 // the scope of JS functions is defined at the time the function is created.//
 //for (pool in pools) {
 var url = 'http://www.ucimu.it/catalogo/imprese/elenco/';
+
     // self-executing function. While a callback function is a function created now but runs later, a self-executing function is created and immediately executed.
     // Whatever this function will returns will be the result of the while expression
-// var companies = [];
+
 request(url, ( function() {
   var companies = [];
   return function(err, resp, body) {
@@ -18,15 +19,17 @@ request(url, ( function() {
     $ = cheerio.load(body);
     $('a[href^="catalogo/imprese"]').each(function(i, elem) {
       companies.push($(this).text());
-      console.log($(this).text())|
-      fs.writeFile('ucimu1.txt',companies, function (err) {
-        if (err) throw err;
-//        console.log('It\'s saved' + companies.length);
-      });
     });
-//  return companies;
-  };
-  })());
+    console.log(companies);
+    l = companies.length;
+    console.log(l);
+    require('./function_write.js')('ucimu1.csv', companies);
 
-// fetch data from level-2 sites //
-// for (var i = 1; i <= length(companies)
+//fetch data from level-2 sites //
+    for (var i = 0;i < l; i++) {
+      var url = 'http://www.ucimu.it/catalogo/imprese/v/' + companies[i];
+      console.log(url);
+
+    }
+  };
+})());
