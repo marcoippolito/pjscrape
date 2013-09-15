aaea381cbeb7140e754efd563eff5c58dfcc8da2
Ubuntu <ubuntu@ip-10-164-57-85.ec2.internal>
2013-09-15 14:00:26 +0000

    function_write.js

--- function_write.js
+++ function_write.js
@@ -1,9 +1,14 @@
 var fs = require('fs');
-var destination; var source;
+var destination; var source; var write;
 exports.write = function(destination, source) {
   fs.writeFile('destination',source, function (err) {
     if (err) throw err;
   });
-};
-//companies = ["a", "b", "c", "d"];
-//write('provaa.csv', companies);
+}
+
+var provaa; 
+var companies = ["a", "b", "c", "d"];
+write('provaa.csv', companies);
+
+
+//module.exports = write;
