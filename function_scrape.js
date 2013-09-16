0c421c245c74d851ff7b1d8aac63b3c9d512fdb6
Ubuntu <ubuntu@ip-10-164-57-85.ec2.internal>
2013-09-16 10:12:59 +0000

    function_scrape.js

--- function_scrape.js
+++ function_scrape.js
@@ -6,15 +6,15 @@ var fs = require('fs');
 var csv = require('csv');
 var w = require('./function_write.js');
     // self-executing function. While a callback function is a function created now but runs later, a self-executing function is created and immediately executed.
-var url;
-exports.scrape = function(url) {
+var url; var param;
+exports.scrape = function(url, param) {
   request(url, ( function() {
     var companies = [];
     return function(err, resp, body) {
       if (err)
 	throw err;
       $ = cheerio.load(body);
-      $('a[href^="catalogo/imprese"]').each(function(i, elem) {
+      $(param).each(function(i, elem) {
 	companies.push($(this).text());
       });
       console.log(companies);
@@ -23,5 +23,5 @@ exports.scrape = function(url) {
       w.write('ucimu1.csv', companies);
 
     };
-  })(url));
+  })(url, param));
 };
