423935ae114e1669860bb29d604b796873193830
Ubuntu <ubuntu@ip-10-164-57-85.ec2.internal>
2013-09-17 19:39:20 +0000

    scrape_call.js

--- scrape_call.js
+++ scrape_call.js
@@ -1,4 +1,7 @@
 var s = require('./function_scrape.js');
 var url = 'http://www.ucimu.it/catalogo/imprese/elenco/';
 var param = 'a[href^="catalogo/imprese"]';
-s.scrape(url, param);
+
+s.scrape(url, param, function(companies) {
+  console.log(companies.length);
+});
