0ce89a39b30acc5963c4b0f48206485828997cdc
Ubuntu <ubuntu@ip-10-164-57-85.ec2.internal>
2013-09-19 10:25:31 +0000

    scrape_call.js

--- scrape_call.js
+++ scrape_call.js
@@ -3,5 +3,5 @@
 var s = require('./function_scrape.js');
 var url = 'http://www.ucimu.it/catalogo/imprese/elenco/';
 var param = 'a[href^="catalogo/imprese"]';
-
+//s.scrape(url, param);
 console.log('Ecco il contenuto del mio array', s.getArray);
