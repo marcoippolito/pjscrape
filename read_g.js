c6e0734ed7df6e93917497bf91f29a3108a1742d
Ubuntu <ubuntu@ip-10-185-5-83.ec2.internal>
2013-10-08 21:27:29 +0000

    read_g.js

--- read_g.js
+++ read_g.js
@@ -1,1 +1,19 @@
-/home/ubuntu/read_g.js
+var fs = require('fs');
+var companyLinks = [];
+var outputfile;
+
+function fromfiletoarray (outputfile) {
+
+  fs.readFile(outputfile, 'utf8', function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    companyLinks = data.toString().split(',');
+    console.log(companyLinks.length);
+    console.log(companyLinks);
+  });
+};
+//var outputfile = './outputdata.csv';
+//fromfiletoarray(outputfile);
+
+module.exports.fromfiletoarray = fromfiletoarray;
