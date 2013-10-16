72427d244b9114e500026665b5ccbad43ab6edc5
Ubuntu <ubuntu@ip-10-185-5-83.ec2.internal>
2013-10-16 06:01:57 +0000

    2lev.js

--- 2lev.js
+++ 2lev.js
@@ -16,26 +16,27 @@ var url ='http://www.ucimu.it/catalogo/imprese/v/cosberg/';
 
 request(url, querySelector);
 
-
-
-
 function querySelector (err, resp, body) {
   var company = new Company();
   if (err)
     throw err;
   $ = cheerio.load(body);
+  //Extract company's name and headquarter's address//
   companyName = $('.page_title').text();
   console.log(companyName);
   companyMainAddress = $('.street').html();
   var toEl = /<br>/;
   var newAddress = companyMainAddress.replace(toEl, " ");
   console.log(newAddress);
+  //Extract name and address of each branch //
+  // First detect number and names of branches //
   var count = [];
   $('.name').each(function(i, elem) {
     count[i] = $(this).text();
   });
-
-  for (var i = 0; i < count.length; i++) {
+  l = count.length;
+  // Extract and save name and address of each branch of the company //
+  for (var i = 0; i < l; i++) {
     companyBranch[i] = {
       compayBranchName: $('.name').eq(i).text(),
       companyBranchAddress: $('.address').eq(i+1).text()
@@ -44,3 +45,5 @@ function querySelector (err, resp, body) {
   }
 
 }
+
+module.exports.request = request;
