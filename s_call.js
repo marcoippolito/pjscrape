536e68e7b995493f707d76801edd35b8f82ab18c
Ubuntu <ubuntu@ip-10-185-5-83.ec2.internal>
2013-10-22 08:04:20 +0000

    s_call.js

--- s_call.js
+++ s_call.js
@@ -9,32 +9,31 @@ var url = 'http://www.ucimu.it/catalogo/imprese/elenco/';
 var param = 'a[href^="catalogo/imprese"]';
 var myArray =[];
 var outputfile = 'ucimulist.csv';
-scrape = function(url, param) {
-  request();
-};
 
-request(url, exports.querySelector);
+scrape = function(callback) {
+  request(url, querySelector);
 
-exports.querySelector = function(err, resp, body) {
-  if (err)
-    throw err;
-  $ = cheerio.load(body);
-  $(param).each(function(i, elem) {
-      myArray.push($(this).text());
-  });
-  for (var i = myArray.length-1; i--;) {
-    if (myArray[i] === 'Catalogo' || myArray[i] === 'Imprese Associate' || myArray[i] === 'Elenco completo' || myArray[i] === 'Elenco Completo' || myArray[i] === '\n\t\t\t\t\t\n\t\t\t\t' || myArray[i] === '\n\t\t\t\t\t\n\t\t\t\t' ) myArray.splice(i, 1);
+  function querySelector (err, resp, body) {
+    if (err)
+      throw err;
+    $ = cheerio.load(body);
+    $(param).each(function(i, elem) {
+	myArray.push($(this).text());
+	exports.getArray = myArray;
+    });
+    for (var i = myArray.length-1; i--;) {
+      if (myArray[i] === 'Catalogo' || myArray[i] === 'Imprese Associate' || myArray[i] === 'Elenco completo' || myArray[i] === 'Elenco Completo' || myArray[i] === '\n\t\t\t\t\t\n\t\t\t\t' || myArray[i] === '\n\t\t\t\t\t\n\t\t\t\t' ) myArray.splice(i, 1);
+    }
+
+//  fs.writeFile(outputfile, myArray, function (err) {
+//    if (err) throw err;
+//    console.log('It\'s saved in: ' + outputfile);
+//  });
+//  console.log('lunghezza di myArray:', myArray.length);
+      callback();
   }
-  exports.getArray = myArray;
-//  console.log(myArray);
-//  var l= myArray.length;
-//  console.log(l);
-  fs.writeFile(outputfile, myArray, function (err) {
-    if (err) throw err;
-    console.log('It\'s saved in: ' + outputfile);
-  });
 };
+//var buffer = './ucimulist.csv';
+//readwrite.fromfiletoarray(buffer);
 
-var buffer = './ucimulist.csv';
-
-readwrite.fromfiletoarray(buffer);
+module.exports.scrape = scrape;
