6e9528fe1d19e7980dabc7523474ff959cbb6810
Ubuntu <ubuntu@ip-10-185-5-83.ec2.internal>
2013-10-23 10:39:30 +0000

    boh.js

--- boh.js
+++ boh.js
@@ -4,12 +4,19 @@ var s_call = require('./s_call.js');
 var natural = require('natural');
 var scrape = s_call.scrape;
 var url = [];
-var tokenizer = new natural.WordTokenizer({pattern: /\s/});
+var tokenizer = new natural.WordTokenizer({pattern: /\s/}); //token are separated by white spaces //
 s_call.scrape (function () {
-  for (var i =0; i < s_call.getArray.length; i++) {
+  // tokenization of s_call.getArray //
+  for (var i = 0; i < s_call.getArray.length; i++) {
     console.log(s_call.getArray[i]);
-    console.log(tokenizer.tokenize(s_call.getArray[i]));
-
-
+    var tokenized = tokenizer.tokenize(s_call.getArray[i]);
+    console.log(tokenized);
+    console.log(tokenized.length);
+    var tokenized_new = [];
+    for (var j = 0; j < tokenized.length; j++) {
+      var toEl = /\./g;
+      tokenized_new[j] = tokenized[j].replace(toEl, "");
+    }
+    console.log(tokenized_new);
   }
 });
