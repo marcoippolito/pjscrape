14e8288b53401663479ae8db032009e5c342e9fd
Ubuntu <ubuntu@ip-10-185-5-83.ec2.internal>
2013-10-23 09:32:16 +0000

    boh.js

--- boh.js
+++ boh.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 
 var s_call = require('./s_call.js');
+var natural = require('natural');
 var scrape = s_call.scrape;
-
+var url = [];
+var tokenizer = new natural.WordTokenizer({pattern: /\s/});
 s_call.scrape (function () {
   for (var i =0; i < s_call.getArray.length; i++) {
-      console.log('Ecco il contenuto del mio array: ', s_call.getArray[i]);
+    console.log(s_call.getArray[i]);
+    console.log(tokenizer.tokenize(s_call.getArray[i]));
+
+
   }
 });
