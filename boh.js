894adbd42a54d54a5bae5bed1663ad56b5ce55f6
Ubuntu <ubuntu@ip-10-185-5-83.ec2.internal>
2013-10-23 19:53:00 +0000

    boh.js

--- boh.js
+++ boh.js
@@ -20,7 +20,7 @@ s_call.scrape (function () {
       tokenized_new[j] = tokenized[j].replace(toEl, "");
     // rules for each number of tokens //
       switch (m) {
-        case '2':
+	case '2':
 	  url = ndarray.matrix(n, 3, 'a');
  	  for (var k = 0; k < n; k++) {
 	    url[k][0] = 'http://www.ucimu.it/en/catalogue/companies/v/' + tokenized_new[0];
@@ -30,36 +30,36 @@ s_call.scrape (function () {
 	    url[k][2] = 'http://www.ucimu.it/en/catalogue/companies/v/' + tokenized_new[0] + '-' + tokenized_new[1];
 	    console.log(url[k][2]);
 	  }
-          break;
-        case '>2':
+	  break;
+	case '>2':
      	  url = ndarray.matrix(n, 6, 'b');
-//	for (var q = 0; q < n; q++) {
-//	  url[q][0] = 'http://www.ucimu.it/en/catalogue/companies/v/' + tokenized_new[0];
-//	  console.log(url[q][0]);
-//	  url[q][1] = 'http://www.ucimu.it/en/catalogue/companies/v/' + tokenized_new[1];
-//	  console.log(url[q][1]);
-//	  url[q][2] = 'http://www.ucimu.it/en/catalogue/companies/v/' + tokenized_new[2];
-//	  console.log(url[q][2]);
-//	  url[q][3] = 'http://www.ucimu.it/en/catalogue/companies/v/' + tokenized_new[0] + '-' + tokenized_new[1];
-//	  console.log(url[q][3]);
-//	  url[q][4] = 'http://www.ucimu.it/en/catalogue/companies/' + tokenized_new[m-1];
-//	  console.log(url[q][4]);
-//	  url[q][5] = 'http://www.ucimu.it/en/catalogue/companies/v/' + tokenized_new[m];
-//	  console.log(url[q][5]);
-//	}
-        default:
+  	  for (var q = 0; q < n; q++) {
+	    url[q][0] = 'http://www.ucimu.it/en/catalogue/companies/v/' + tokenized_new[0];
+	    console.log(url[q][0]);
+ 	    url[q][1] = 'http://www.ucimu.it/en/catalogue/companies/v/' + tokenized_new[1];
+	    console.log(url[q][1]);
+	    url[q][2] = 'http://www.ucimu.it/en/catalogue/companies/v/' + tokenized_new[2];
+	    console.log(url[q][2]);
+	    url[q][3] = 'http://www.ucimu.it/en/catalogue/companies/v/' + tokenized_new[0] + '-' + tokenized_new[1];
+	    console.log(url[q][3]);
+	    url[q][4] = 'http://www.ucimu.it/en/catalogue/companies/' + tokenized_new[m-1];
+ 	    console.log(url[q][4]);
+	    url[q][5] = 'http://www.ucimu.it/en/catalogue/companies/v/' + tokenized_new[m];
+	    console.log(url[q][5]);
+	  }
+	default:
 	  url = ndarray.matrix(n, 1, 'c');
 	  console.log(url);
-//	for (var k =0; k < n; k++) {
-//	  url[k][0] = 'http://www.ucimu.it/en/catalogue/companies/v/' + tokenized_new[0];
-//	  console.log(url[k][0]);
-//	}
+	  for (var k =0; k < n; k++) {
+	    url[k][0] = 'http://www.ucimu.it/en/catalogue/companies/v/' + tokenized_new[0];
+	    console.log(url[k][0]);
+	  }
       }
       console.log(tokenized_new);
       console.log(m);
     }
   }
-//    console.log(tokenized_new);
-//    console.log(tokenized_new.length);
+    console.log(tokenized_new);
+    console.log(tokenized_new.length);
 
 });
