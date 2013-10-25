99723eed8ee05c7064a23343f34742872b42da85
Ubuntu <ubuntu@ip-10-185-5-83.ec2.internal>
2013-10-25 09:39:45 +0000

    2lev.js

--- 2lev.js
+++ 2lev.js
@@ -3,12 +3,10 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
-var company_object = require('./company_object');
-var Company = company_object.Company;
-var companyBranch = [];
-
+//var company_object = require('./company_object');
+//var createcompany = company_object.createcompany;
+var companyMainAddress; var companyBranchName; var companyBranchAddress;
 var url ='http://www.ucimu.it/catalogo/imprese/v/cosberg/';
-//var param = 'street';
 
 //scrape = function(url, param) {
 //  request();
@@ -17,17 +15,29 @@ var url ='http://www.ucimu.it/catalogo/imprese/v/cosberg/';
 request(url, querySelector);
 
 function querySelector (err, resp, body) {
-  var company = new Company();
+
+  function createcompany (){
+    this.companyName = companyName;
+    this.companyMainAddress = companyMainAddress;
+    this.companyBranch = createcompanyBranch();
+  }
+
+  function createcompanyBranch() {
+    this.companyBranch = companyBranch;
+    this.companyBranchName = companyBranchName;
+    this.companyBranchAddress = companyBranchAddress;
+  }
+
   if (err)
     throw err;
   $ = cheerio.load(body);
   //Extract company's name and headquarter's address//
-  companyName = $('.page_title').text();
+  companyName = $('.page_title').text(); //companyName//
   console.log(companyName);
   companyHeadquarter = $('.street').html();
   var toEl = /<br>/;
-  var companyMainAddress = companyHeadquarter.replace(toEl, " ");
-  console.log(companyMainAddress);
+  var companyMainAddress = companyHeadquarter.replace(toEl, " "); //companyMainAddress//
+//  console.log(companyMainAddress);
   //Extract name and address of each branch //
   // First detect number and names of branches //
   var count = [];
@@ -36,16 +46,18 @@ function querySelector (err, resp, body) {
   });
   l = count.length;
   // Extract and save name and address of each branch of the company //
+  var companyBranch = [];
   for (var i = 0; i < l; i++) {
     companyBranch[i] = {
       companyBranchName: $('.name').eq(i).text(),
       companyBranchAddress: $('.address').eq(i+1).text()
     };
-  console.log(companyBranch[i]);
-  exports.companyName = companyName;
-  exports.companyHeadquarter = companyHeadquarter;
-  exports.getObject = companyBranch[i];
+    companyBranch.push(companyBranch[i]);
   }
+//  console.log(companyBranch);
+
+  var company = new createcompany(companyName, companyMainAddress, companyBranch);
+  console.log(company);
 
 }
 
