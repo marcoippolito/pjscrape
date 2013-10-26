daddd82ee2f8a5abe48de8fcf62959dde95acd12
Ubuntu <ubuntu@ip-10-185-5-83.ec2.internal>
2013-10-26 17:12:10 +0000

    2lev.js

--- 2lev.js
+++ 2lev.js
@@ -5,7 +5,7 @@ var cheerio = require('cheerio');
 var fs = require('fs');
 //var company_object = require('./company_object');
 //var createcompany = company_object.createcompany;
-var company; var companyMainAddress; var companyBranchName; var companyBranchAddress;
+var company; var companyBranchName; var companyBranchAddress;
 var url ='http://www.ucimu.it/catalogo/imprese/v/cosberg/';
 
 //scrape = function(url, param) {
@@ -18,7 +18,7 @@ function querySelector (err, resp, body) {
 
   function createcompany (){
     this.companyName = companyName;
-    this.companyMainAddress = companyMainAddress;
+    this.companyHeadquarter = companyHeadquarter;
     this.companyBranch = companyBranch;
   }
 
@@ -27,10 +27,13 @@ function querySelector (err, resp, body) {
   $ = cheerio.load(body);
   //Extract company's name and headquarter's address//
   companyName = $('.page_title').text(); //companyName//
+  var torm = /\n|\t/g;
+  var companyName = companyName.replace(torm, '');
 //  console.log(companyName);
   companyHeadquarter = $('.street').html();
-  var toEl = /<br>/;
-  var companyMainAddress = companyHeadquarter.replace(toEl, " "); //companyMainAddress//
+  var toEl = /<br>|\\\s/g;
+  var companyHeadquarter = companyHeadquarter.replace(toEl, '' ); //companyMainAddress//
+
 //  console.log(companyMainAddress);
   //Extract name and address of each branch //
   // First detect number and names of branches //
@@ -47,8 +50,8 @@ function querySelector (err, resp, body) {
       companyBranchAddress: $('.address').eq(i+1).text()
     };
   }
-  // Assign to the variable "company" the data just extracted //
-  var company = new createcompany(companyName, companyMainAddress, companyBranch);
+  // Assign the data just extraced to the variable "company" //
+  var company = new createcompany(companyName, companyHeadquarter, companyBranch);
   console.log(company);
 
 }
