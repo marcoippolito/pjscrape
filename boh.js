e5ee1a007b892a5f59d1030fd7037f90888102dd
Ubuntu <ubuntu@ip-10-185-5-83.ec2.internal>
2013-10-29 13:38:13 +0000

    boh.js

--- boh.js
+++ boh.js
@@ -11,36 +11,36 @@ var tokenizer = new natural.WordTokenizer({pattern: /\s/}); //tokens are separat
 var url = [];
 var urls_array = [];
 
-//wrapper = function (cb) {
-scrape (function callback() {
-  // tokenization of s_call.getArray //
-  var n = s_call.getArray.length;
-  for (var i = 0; i < n; i++) {
-//    console.log(s_call.getArray[i]);
-    var tokenized = tokenizer.tokenize(s_call.getArray[i]);
-    var m = tokenized.length;
-    var tokenized_new = [];
-    // remove "." within each token //
-    for (var j = 0; j < m; j++) {
-      var toEl = /\./g;
-      tokenized_new[j] = tokenized[j].replace(toEl, "");
-    //All possible urls are generated and saved in "urls_array" array //
-      url[i] = 'http://www.ucimu.it/en/catalogue/companies/v/' + tokenized_new[j];
-      urls_array.push(url[i]);
-      url[i] = 'http://www.ucimu.it/en/catalogue/companies/v/' + tokenized_new[0] + '-' + tokenized_new[1];
-      urls_array.push(url[i]);
-
+wrapper = function (cb) {
+  scrape (function callback() {
+    // tokenization of s_call.getArray //
+    var n = s_call.getArray.length;
+    for (var i = 0; i < n; i++) {
+  //    console.log(s_call.getArray[i]);
+      var tokenized = tokenizer.tokenize(s_call.getArray[i]);
+      var m = tokenized.length;
+      var tokenized_new = [];
+      // remove "." within each token //
+      for (var j = 0; j < m; j++) {
+	var toEl = /\./g;
+	tokenized_new[j] = tokenized[j].replace(toEl, "");
+      //All possible urls are generated and saved in "urls_array" array //
+	url[i] = 'http://www.ucimu.it/en/catalogue/companies/v/' + tokenized_new[j];
+	urls_array.push(url[i]);
+	url[i] = 'http://www.ucimu.it/en/catalogue/companies/v/' + tokenized_new[0] + '-' + tokenized_new[1];
+	urls_array.push(url[i]);
+      }
     }
+    var urls_array_new = urls_array.unique();
+    var ul = urls_array_new.length;
+//    console.log(ul);
+//    console.log(urls_array_new);
 
-  }
-  var urls_array_new = urls_array.unique();
-  console.log(urls_array_new);
-  var ul = urls_array_new.length;
-  console.log(ul);
-  console.log(urls_array_new);
+    exports.getArray = urls_array_new;
 
-  exports.getArray = urls_array_new;
-});
+    cb();
 
+  });
+};
 
-module.exports.scrape = scrape;
+module.exports.wrapper = wrapper;
