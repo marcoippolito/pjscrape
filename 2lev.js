5da288daef2e16288406d6d72e2593f19851dda2
Ubuntu <ubuntu@ip-10-185-5-83.ec2.internal>
2013-10-29 09:03:11 +0000

    2lev.js

--- 2lev.js
+++ 2lev.js
@@ -5,7 +5,7 @@ var cheerio = require('cheerio');
 var fs = require('fs');
 //var company_object = require('./company_object');
 var company; var companyBranchName; var companyBranchAddress;
-var url ='http://www.ucimu.it/catalogo/imprese/v/cosberg/';
+var url ='http://www.ucimu.it/en/catalogue/companies/v/';
 
 //scrape = function(url, param) {
 //  request();
@@ -24,33 +24,46 @@ function querySelector (err, resp, body) {
   if (err)
     throw err;
   $ = cheerio.load(body);
-  //Extract company's name and headquarter's address//
-  companyName = $('.page_title').text(); //companyName//
-  var torm = /\n|\t/g;
-  var companyName = companyName.replace(torm, '');
-//  console.log(companyName);
-  companyHeadquarter = $('.street').html();
-  var toEl = /<br>|\\s/g;
-  var companyHeadquarter = companyHeadquarter.replace(toEl, '' ); //companyMainAddress//
-
-  //Extract name and address of each branch //
-  // First detect number and names of branches //
-  var count = [];
-  $('.name').each(function(i, elem) {
-    count[i] = $(this).text();
-  });
-  l = count.length;
-  // Extract and save name and address of each branch of the company //
-  var companyBranch = [];
-  for (var i = 0; i < l; i++) {
-    companyBranch[i] = {
-      companyBranchName: $('.name').eq(i).text(),
-      companyBranchAddress: $('.address').eq(i+1).text()
-    };
+  var h2 = $('.editor').text();
+  console.log(h2);
+  if (h2 == 'Item not found') {
+    return;
   }
+  else {
+    //Extract company's name and headquarter's address//
+    companyName = $('.page_title').text(); //companyName//
+    var torm = /\n|\t/g;
+    var companyName = companyName.replace(torm, '');
+    console.log(companyName);
+    companyHeadquarter = $('.street').html();
+    if (! companyHeadquarter) {
+      return;
+    }
+    else {
+      var toEl = /<br>|\\s/g;
+      // companyHeadquarter //
+      var companyHeadquarter = companyHeadquarter.replace(toEl, '' );
+    }
+
+    //Extract name and address of each branch //
+    //First detect number and names of branches //
+    var count = [];
+    $('.name').each(function(i, elem) {
+      count[i] = $(this).text();
+    });
+    l = count.length;
+    //Extract and save name and address of each branch of the company //
+    var companyBranch = [];
+    for (var i = 0; i < l; i++) {
+      companyBranch[i] = {
+	companyBranchName: $('.name').eq(i).text(),
+	companyBranchAddress: $('.address').eq(i+1).text()
+      };
+    }
   // Assign the data just extraced to the variable "company" //
-  var company = new createcompany(companyName, companyHeadquarter, companyBranch);
-  console.log(company);
+//  var company = new createcompany(companyName, companyHeadquarter, companyBranch);
+//  console.log(company);
+  }
 
 }
 
