0d6f40bb336ba5bd0c5ea499597da2e1774e40b5
Ubuntu <ubuntu@ip-10-185-5-83.ec2.internal>
2013-11-02 08:03:19 +0000

    geonoderprova.js

--- geonoderprova.js
+++ geonoderprova.js
@@ -2,17 +2,27 @@
 
 var geonoder = require('geonoder');
 var address1 = 'PIAZZA DEL DUOMO 5 20121 MILANO MI';
-var address = 'VIA RUFFILLI 8/10 20060 PESSANO CON BORNAGO (MILANO)';
+var address = 'VIA MAGGIO, LOCALITA I CASONI 81-83 29027 PODENZANO (PC)';
 var lat; var long;
 
-geonoder.toCoordinates(address, geonoder.providers.nominatim, function(lat, long) {
-  console.log('Lat nominatim: ' + lat + ' Long nominatim: ' + long); // Lat: 41.8965209 Long: 12.4805225
-  if (lat === null || long === null) {
-    geonoder.toCoordinates(address, geonoder.providers.google, function(lat, long) {
-      console.log('Lat google: ' + lat + ' Long google: ' + long);
-    });
-  }
-  else {
-	return;
-  }
-});
+// geocoordinates through Nominatim (OpenStreetMap) //
+function geoNoder (address) {
+  geonoder.toCoordinates(address, geonoder.providers.nominatim, function(lat, long) {
+    console.log('Lat nominatim: ' + lat + ' Long nominatim: ' + long); // Lat: 41.8965209 Long: 12.4805225
+    if (lat === null || long === null) {
+    // geocoordinates through Google //
+      geonoder.toCoordinates(address, geonoder.providers.google, function(lat, long) {
+	console.log('Lat google: ' + lat + ' Long google: ' + long);
+      });
+    }
+    else {
+      return;
+    }
+  });
+}
+
+geoNoder(address);
+
+module.exports.geoNoder = geoNoder;
+module.exports.lat = lat;
+module.exports.long = long;
