657eb0feb8870328f4f6e605f13ca9e55c5d0837
Ubuntu <ubuntu@ip-10-185-5-83.ec2.internal>
2013-11-07 22:33:01 +0000

    regiaucimu.js

--- regiaucimu.js
+++ regiaucimu.js
@@ -6,11 +6,15 @@ var boh = require('./boh');
 var wrapper = boh.wrapper;
 var fs = require('fs');
 
-boh.wrapper(function() {
+function swrapper () {
+  boh.wrapper(function() {
 
-  for (var k = 0; k < boh.getArray.length; k++) {
-//    console.log(boh.getArray[k]);
-    scrapesl(boh.getArray[k]);
-  }
+    for (var k = 0; k < boh.getArray.length; k++) {
+      scrapesl(boh.getArray[k]);
+    }
+  });
+}
 
-});
+//swrapper();
+
+module.exports.swrapper = swrapper;
