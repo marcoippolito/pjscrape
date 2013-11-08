79efef52efd86bec4a3d7a6aa83e9c41d58ab6eb
Ubuntu <ubuntu@ip-10-185-5-83.ec2.internal>
2013-11-08 15:41:42 +0000

    geonodermaster.js

---
+++ geonodermaster.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+var geonoder = require('geonoder');
+var address = 'VIA E. FERMI 13 28060 SAN PIETRO MOSEZZO (NO)';
+var lat; var long;
+//var address;
+// geocoordinates through Nominatim (OpenStreetMap) //
+function geoNoder (address) {
+  geonoder.toCoordinates(address, geonoder.providers.nominatim, function(lat, long) {
+    console.log('Lat nominatim: ' + lat + ' Long nominatim: ' + long); // Lat: 41.8965209 Long: 12.4805225
+    if (lat === null || long === null) {
+    // geocoordinates through Google //
+      geonoder.toCoordinates(address, geonoder.providers.google, function(lat, long) {
+	console.log('Lat google: ' + lat + ' Long google: ' + long);
+      });
+    }
+    else {
+      return;
+    }
+  });
+}
+
+geoNoder(address);
+
+module.exports.geoNoder = geoNoder;
+module.exports.lat = lat;
+module.exports.long = long;
